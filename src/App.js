599b634b3f7db1dc2aeac860887bfc9598da9608
PreetChauhan111 <preetchauhan421@gmail.com>
2024-02-26 18:59:27 +0530

    add collage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> preet milan
+          Edit <code>src/App.js</code> preet milan charusat
         </p>
         <a
           className="App-link"
