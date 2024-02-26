491460c9855b8843501670302ddef11c0212aae4
milan-bavishi <milanbavishi99@gmail.com>
2024-02-26 18:57:47 +0530

    add name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> preet
+          Edit <code>src/App.js</code> preet milan
         </p>
         <a
           className="App-link"
