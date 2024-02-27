4f4de768cf6c7e6e9a0471e4984401cc1a5ba593
milan-bavishi <milanbavishi99@gmail.com>
2024-02-27 18:26:03 +0530

    Add Review Section

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
-import './App.css';
-import eventeco from './icon/eventeco.png'
-import logo from './icon/logo-nav-qr.png';
-import hero from './icon/hero-image.svg';
-import image from './icon/image.png';
-import image2 from './icon/image2.png';
-import image3 from './icon/image3.png';
-import Ellipse1 from './icon/Ellipse1.svg';
-import Ellipse2 from './icon/Ellipse2.svg';
-import Ellipse3 from './icon/Ellipse3.svg';
-import Vector from './icon/Vector.svg';
-import yellowVector from './icon/yellow-vector.svg';
-import connectGraphic from './icon/connect-graphic.svg';
-import connectMail from './icon/connect-mail.svg';
-import github from './icon/github.svg';
-import instagram from './icon/instagram.svg';
-import linkedin from './icon/linkedin.svg';
-import location from './icon/location.svg';
-import mail from './icon/mail.svg';
-import mailSubscribe from './icon/mail-subscribe.svg';
-import number from './icon/number.svg';
-import reviewElemant from './icon/review-elemant.svg';
-import reviewEllipse1 from './icon/review-ellipse-1.svg';
-import reviewEllipse2 from './icon/review-ellipse-2.svg';
-import reviewEllipse3 from './icon/review-ellipse-3.svg';
-import reviewEllipse4 from './icon/review-ellipse--4.svg';
-import reviewQutos from './icon/review-qutos.svg';
-import subscribe from './icon/subscribe.svg';
-import teamSemicir from './icon/team-semi-cir.svg';
-import Vector1 from './icon/Vector-1.svg';
-import Vector2 from './icon/Vector-2.svg';
-import whyusEle1 from './icon/whyus-ele-1.svg';
-import whyusEle2 from './icon/whyus-ele-2.svg';
+import "./App.css";
+import eventeco from "./icon/eventeco.png";
+import logo from "./icon/logo-nav-qr.png";
+import hero from "./icon/hero-image.svg";
+import image from "./icon/image.png";
+import image2 from "./icon/image2.png";
+import image3 from "./icon/image3.png";
+import Ellipse1 from "./icon/Ellipse1.svg";
+import Ellipse2 from "./icon/Ellipse2.svg";
+import Ellipse3 from "./icon/Ellipse3.svg";
+import Vector from "./icon/Vector.svg";
+import yellowVector from "./icon/yellow-vector.svg";
+import connectGraphic from "./icon/connect-graphic.svg";
+import connectMail from "./icon/connect-mail.svg";
+import github from "./icon/github.svg";
+import instagram from "./icon/instagram.svg";
+import linkedin from "./icon/linkedin.svg";
+import location from "./icon/location.svg";
+import mail from "./icon/mail.svg";
+import mailSubscribe from "./icon/mail-subscribe.svg";
+import number from "./icon/number.svg";
+import reviewElemant from "./icon/review-elemant.svg";
+import reviewEllipse1 from "./icon/review-ellipse-1.svg";
+import reviewEllipse2 from "./icon/review-ellipse-2.svg";
+import reviewEllipse3 from "./icon/review-ellipse-3.svg";
+import reviewEllipse4 from "./icon/review-ellipse--4.svg";
+import reviewQutos from "./icon/review-qutos.svg";
+import subscribe from "./icon/subscribe.svg";
+import teamSemicir from "./icon/team-semi-cir.svg";
+import Vector1 from "./icon/Vector-1.svg";
+import Vector2 from "./icon/Vector-2.svg";
+import whyusEle1 from "./icon/whyus-ele-1.svg";
+import whyusEle2 from "./icon/whyus-ele-2.svg";
 
 function App() {
   return (
     <>
-    <div className="wrapper">
+      <div className="wrapper">
         {/* Header */}
         <header>
           {/* NavBar */}
           <nav>
             {/* Logo */}
             <div className="event">
-              <img src={eventeco} alt="" id="eventechoLogo"/>
+              <img src={eventeco} alt="" id="eventechoLogo" />
             </div>
             {/* Componen */}
             <div className="components">
               <ul>
                 <li>
                   <a href="#">Home</a>
-                  </li>
+                </li>
                 <li>
                   <a href="#">Why US</a>
-                  </li>
+                </li>
                 <li>
                   <a href="#">Our Team</a>
-                  </li>
+                </li>
                 <li>
                   <a href="#">Review</a>
-                  </li>
+                </li>
                 <li>
                   <a href="#">Contact Us</a>
-                  </li>
+                </li>
               </ul>
             </div>
             {/* Button */}
@@ -77,76 +77,76 @@ function App() {
               <div>
                 <h1>Your Event, Safety Ours!</h1>
                 <h2>Safe Event No Stress</h2>
-              </div> 
+              </div>
               <div>
                 <button>Create Our Event </button>
               </div>
             </div>
-            
+
             <div className="content">
               <div>
-              <h1>Everything An Event Needs</h1>
-              <p>
-                Watch your visions of the perfect event come to life with
-                EvenTor. From meticulous planning to foolproof execution –
-                we will ensure your event gets everything it needs with top priority.
-              </p>
+                <h1>Everything An Event Needs</h1>
+                <p>
+                  Watch your visions of the perfect event come to life with
+                  EvenTor. From meticulous planning to foolproof execution – we
+                  will ensure your event gets everything it needs with top
+                  priority.
+                </p>
               </div>
             </div>
-            <img src={Ellipse3} id='ellipse3' loading='lazy'/>
-            <img src={Ellipse1} id='ellipse1' loading='lazy' />
-            <img src={Ellipse2} id='ellipse2' loading='lazy'/>
-            <img src={image} id='image' />
-            <img src={image2} id='image2'/>
-            <img src={image3} id='image3'/>
-            <img src={hero} id='hero'/> 
-            <img src={ yellowVector} id='yellowVector' />
-            <img src={Vector} id='vector' />
+            <img src={Ellipse3} id="ellipse3" loading="lazy" />
+            <img src={Ellipse1} id="ellipse1" loading="lazy" />
+            <img src={Ellipse2} id="ellipse2" loading="lazy" />
+            <img src={image} id="image" />
+            <img src={image2} id="image2" />
+            <img src={image3} id="image3" />
+            <img src={hero} id="hero" />
+            <img src={yellowVector} id="yellowVector" />
+            <img src={Vector} id="vector" />
           </section>
-
 
           {/* Why us */}
-          <section className='whyUs'>
-              <div className='dots'>
-                <img src="" alt="" />
-                <h1>Why Us</h1>
-                <img src="" alt="" />
+          <section className="whyUs">
+            <div className="dots">
+              <img src={whyusEle1} id="dot1" />
+              <h1>Why Us</h1>
+              <img src={whyusEle2} id="dot2" />
+            </div>
+            <div className="cards">
+              <img src={Vector1} id="vector1" />
+              <img src={Vector2} id="vector2" />
+
+              <div className="cardMain">
+                <div className="first">
+                  <div className="card">Reliable Support</div>
+                  <div className="card">Scalability</div>
+                </div>
+
+                <div className="second">
+                  <div className="card">Innovative Technology</div>
+                  <div className="card">Ease to Integration</div>
+                  <div className="card">Continuous Innovation</div>
+                </div>
+
+                <div className="third">
+                  <div className="card">Expert Team</div>
+                  <div className="card">Key Features</div>
+                </div>
               </div>
-              <div className='cards'>
-                <img src={Vector1}  id='vector1'/>
-                <img src={Vector2}  id='vector2'/>
-                
-                
-                
-                {/* <div className="card1">
-                  Innovative Technology 
-                </div>
-                <div className="card2">
-                Reliable Support
-                </div>
-                <div className="card3">
-                Ease to Integration
-
-                </div>
-                <div className="card4">
-                Expert Team
-                </div>
-                <div className="card5">
-                Scalability
-                </div>
-                <div className="card6">
-                Continuous Innovation
-                </div>
-                <div className="card7">
-                Key Features
-                </div> */}
-
-
-              </div>
+            </div>
           </section>
 
+          <section>
+            <div className="mainreview">
+              <div className="heading">
+                <div>Hear From Our Happy Clients</div>
+              </div>
+
+              <div className="review"></div>
+            </div>
+          </section>
         </main>
-       </div>
+      </div>
     </>
   );
 }
